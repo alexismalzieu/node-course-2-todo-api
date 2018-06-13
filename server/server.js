b0119a0e6252ba0a4da8a6742d2ef1d1c88cbe8b
alexismalzieu <alex.malzieu@gmail.com>
2018-06-13 18:01:04 +0200

    Add POST todos route

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require("./db/mongoose");
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req, res) =>{
+    console.log(req.body);
+
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    })
+
+});
+
+app.listen(5000, ()=>{
+    console.log('Started listening on port 5000');
+})
